e81a785b820e098019f4b1e43ae452eb5cd9fec3
Your Name <you@example.com>
2022-04-05 17:09:55 +0300

    project init

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import  express from "express";
 import cors from 'cors'
-// import { customersRouter } from "./controllers/customers-controller.js";
-
+// import { medCompaniesRouter } from "./controllers/medCompanies-controller.js";
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-// app.use('/api', customersRouter)
+// app.use('/api', medCompaniesRouter)
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
